977e201b9a3240eeafa8e8dcaaedcc53b45a3fbe
Pedro Miguel Mourão Martins <miguelrox@msn.com>
2018-04-03 08:11:23 +0000

    Update model

--- ToggleAPI/Models/ViewModels/ToggleServiceViewModel.cs
+++ ToggleAPI/Models/ViewModels/ToggleServiceViewModel.cs
@@ -6,11 +6,9 @@ namespace ToggleAPI.Models
     public class ToggleServiceViewModel
     {
         private TogglesServiceDto togglesServiceDto;
-        public ServiceViewModel ServiceViewModel { get; set; }
         public List<ToggleViewModel> ToggleViewModels { get; set; }
         public ToggleServiceViewModel(TogglesServiceDto togglesServiceDto)
         {
-            ServiceViewModel = new ServiceViewModel(togglesServiceDto.ServiceDto);
             ToggleViewModels = new List<ToggleViewModel>();
             foreach (var toggleDto in togglesServiceDto.TogglesDtos)
             {
